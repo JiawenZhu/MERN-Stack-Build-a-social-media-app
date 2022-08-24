5de46e9e8bd0c8e2c9721d0e219ebedf263b04a9
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2022-08-24 09:43:22 -0500

    some overall fix

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String,
-        default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+        default: 'https://res.cloudinary.com/pengushe-home/image/upload/v1661056716/cld-sample-5.jpg'
     },
     role: {type: String, default: 'user'},
     gender: {type: String, default: 'male'},
