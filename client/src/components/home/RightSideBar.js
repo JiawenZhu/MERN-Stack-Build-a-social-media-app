226f88d59885a77a25d118b759d4aa111fc76ac4
Jiawen  Zhu <jiawenzhu@MacBook-Air.local>
2022-09-29 14:03:52 -0500

    add names of user on follower and following

--- client/src/components/home/RightSideBar.js
+++ client/src/components/home/RightSideBar.js
@@ -15,7 +15,7 @@ const RightSideBar = () => {
             <UserCard user={auth.user} />
 
             <div className="d-flex justify-content-between align-items-center my-2">
-                <h5 className="text-danger">新添加的朋友们！</h5>
+                <h7 className="text-danger">关注后就可以看到他们发的帖子（Post）！</h7>
                 {
                     !suggestions.loading &&
                     <i className="fas fa-redo" style={{cursor: 'pointer'}}
--- client/src/components/profile/Followers.js
+++ client/src/components/profile/Followers.js
@@ -8,7 +8,7 @@ const Followers = ({users, setShowFollowers}) => {
     return (
         <div className="follow">
             <div className="follow_box">
-                <h5 className="text-center">关注我的</h5>
+                <h5 className="text-center">关注❤️{auth.user.username}❤️的</h5>
                 <hr/>
                 
                 <div className="follow_content">
